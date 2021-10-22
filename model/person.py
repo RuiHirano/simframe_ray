e17c6f49eb8ed2c31e9b3f2ff4360b9a59196483
Rui Hirano <r.hrn.0930@gmail.com>
2021-10-22 18:00:44 +0900

    add model car and person

---
+++ model/person.py
@@ -0,0 +1,13 @@
+from lib.agent import IAgent, Agent, Position
+from typing import List
+
+class Person(Agent):
+    def __init__(self, id: str, position: Position):
+        super().__init__(id, position, type="Person")
+        self.interaction_range = 1
+
+    def step(self, neighbors: List[IAgent]):
+        self.position.set_x(self.position.x - 5)
+        self.position.set_y(self.position.y + 5)
+        print("Step Agent (ID: {}) Pos: X:{}, Y: {}".format(self.id, self.position.x, self.position.y))
+
