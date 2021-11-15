04cc7565903c163e43da3e9c0aec5f5a8cf20cdc
Rui Hirano <r.hrn.0930@gmail.com>
2021-11-15 18:40:48 +0900

    update cosim

--- examples/co_simulation/simframe/engine.py
+++ examples/co_simulation/simframe/engine.py
@@ -66,7 +66,7 @@ class Engine:
 
         full_msg = b''
         while True:
-            msg = s.recv(1024)
+            msg = s.recv(4096)
             if len(msg) <= 0:
                 break
             full_msg += msg
